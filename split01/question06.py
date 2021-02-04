fda164eb312c593b2c185f0b988d401a35150e65
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-04 17:30:49 +0200

    Question 6 - Solved

---
+++ split01/question06.py
@@ -0,0 +1,19 @@
+def order(a: list) -> str:
+    ascending = True
+    descending = True
+    for i in range(len(a) - 1):
+        if not (a[i] >= a[i + 1] and descending):
+            descending = False
+        
+        if not (a[i] <= a[i + 1] and ascending):
+            ascending = False
+
+    if ascending:
+        return 'ascending'
+    elif descending:
+        return 'descending'
+    else:
+        return 'not sorted'
+
+
+print(order([1, 7, 0, 4, 8, 1]))
