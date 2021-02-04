14ad9849ec1374b0a3101021c9d4d9110801dd7d
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-04 23:38:23 +0200

    Question 7 - Solved

---
+++ split01/question07.py
@@ -0,0 +1,18 @@
+def Cipher_Zeroes(N):
+    zeros = {'0': 1, '6': 1, '9': 1, '8': 2}
+    counter = 0
+
+    for num in N:
+        if num in zeros:
+            counter += zeros[num]
+
+    if counter == 0:
+        return '0'
+
+    if counter % 2 != 0:
+        return str(bin(counter + 1))[2:]
+    else:
+        return str(bin(counter - 1))[2:]
+
+
+print(Cipher_Zeroes('4'))
