93b424c635cce3b677ed387f73b94db42a011a09
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-05 20:31:57 +0200

    Question 4 - better solution found

--- split01/question04.py
+++ split01/question04.py
@@ -1,6 +1,2 @@
-# not solved yet
 def findPermutation(n: int, p: list, q: list) -> list:
     return [p.index(q[x]) + 1 for x in range(n)]
-
-
-print(findPermutation(5, [3, 4, 1, 2, 5], [4, 5, 2, 3, 1]))
