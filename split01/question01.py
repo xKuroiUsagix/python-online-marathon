558f209c0211285a6d66f20818a8d89d35aa9e36
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-06 11:29:40 +0200

    Question 5 - Solved

--- split01/question01.py
+++ split01/question01.py
@@ -7,10 +7,6 @@ def kthTerm(n: int, k: int) -> int:
         for j in temp:
             pows.append(n**i + j)
 
-        print(pows)
         i += 1
         if len(pows) >= k:
             return pows[k - 1]
-
-
-print(kthTerm(3, 7))
--- split01/question05.py
+++ split01/question05.py
@@ -1,18 +1,44 @@
 def toPostFixExpression(expression: list) -> list:
-    operators = ['-', '+', '*', '/', '%']
+    operators = {'+': 0, '-': 0, '*': 1, '/': 1, '%': 1}
     open_p, close_p = '(', ')'
-    output = []
-    output_operators = []
+    postfix = []
+    stack = []
 
-    for number in expression:
-        if number not in operators:
-            output.append(number)
-        else:
-            output_operators.append(number)
+    for i in expression:
+        try:
+            if i not in operators.keys() and i not in [open_p, close_p]:
+                postfix.append(i)
+            elif operators.get(stack[-1], -1) < operators.get(i, -1) or stack[-1] == open_p:
+                stack.append(i)
+            else:
+                while len(stack) and operators.get(stack[-1]) >= operators.get(i, 2)\
+                                and stack[-1] not in [open_p, close_p]:
+                    postfix.append(stack.pop())
+                stack.append(i)
+        except IndexError:
+            stack.append(i)
+            continue
 
-    return output + output_operators
+        if i == close_p:
+            while len(stack) and stack[-1] != '(':
+                if stack[-1] in [open_p, close_p]:
+                    stack.pop()
+                else:
+                    postfix.append(stack.pop())
+            stack.pop()
+
+    while len(stack):
+        postfix.append(stack.pop())
+
+    return postfix
 
 
-print(toPostFixExpression(['2', '+', '3']))
-# 20 + 3 * (5 * 4) = 20 3 5 4 * * +
-# 
+print(toPostFixExpression(["20",
+                           "+",
+                           "3",
+                           "*",
+                           "(",
+                           "5",
+                           "*",
+                           "4",
+                           ")"]))
