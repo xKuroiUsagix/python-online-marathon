d286448346bdb77d402a7aedc2a8825d20fc3531
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-08 18:05:06 +0200

    Question 1 - better solution found

--- split02/question01.py
+++ split02/question01.py
@@ -1,17 +1,3 @@
-def double_string(data: list) -> int:
-    counter = 0
-    for i in data:
-        center = int(len(i)/2)
-        connected_left = i[:center]
-        connected_right = i[center:]
-        
-        if connected_left in data and connected_right in data:
-            counter += 1
-
-    return counter
-
 def ds(data: list) -> int:
-    return len([x for x in data if x[:int(len(x)/2)] in data and x[int(len(x)/2):] in data])
-
-
-print(ds(['aa', 'aaaa', 'aaaaaaaa', 'aaaa', 'qwer', 'qweraaaa']))
+    return len([x for x in data if x[:int(len(x)/2)] in data and\
+                                   x[int(len(x)/2):] in data])
