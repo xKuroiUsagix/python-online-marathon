3d4cd5f830c4b15448ed63a44d403b50de43040c
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-08 18:10:24 +0200

    Question 5 - deleted unnecessary print()

--- split02/question05.py
+++ split02/question05.py
@@ -11,14 +11,3 @@ def max_population(data: list) -> tuple:
             max_city = city_population[0]
 
     return max_city, max_pop
-
-
-print(max_population(["id,name,poppulation,is_capital",
-"3024,eu_kyiv,24834,y",
-"3025,eu_volynia,20231,n",
-"3026,eu_galych,23745,n",
-"4892,me_medina,18038,n",
-"4401,af_cairo,18946,y",
-"4700,me_tabriz,13421,n",
-"4899,me_bagdad,22723,y",
-"6600,af_zulu,09720,n"]))
