ec8ff8a751406cd85bf38b3048dc4095c4861cc0
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-08 18:07:12 +0200

    Question 1 - function renamed to double_string

--- split02/question01.py
+++ split02/question01.py
@@ -1,3 +1,3 @@
-def ds(data: list) -> int:
+def double_string(data: list) -> int:
     return len([x for x in data if x[:int(len(x)/2)] in data and\
                                    x[int(len(x)/2):] in data])
