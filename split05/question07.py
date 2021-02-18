d05c8c22e72430a80f5e786a7cede18428345559
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-18 13:07:35 +0200

    Question 7 - Solved

---
+++ split05/question07.py
@@ -0,0 +1,7 @@
+def divide(numerator, denominator):
+    try:
+        return f'Result is {numerator / denominator}'
+    except ZeroDivisionError:
+        return f'Oops, {numerator} / {denominator}, division by zero is error!!!'
+    except TypeError:
+        return 'Value Error! You did not enter a number!'
