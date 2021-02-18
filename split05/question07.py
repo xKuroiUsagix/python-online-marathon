c9a760800623f1a74af9d4e2e70a12f21335bd91
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-18 13:08:09 +0200

    Deleted few spaces

--- split05/question07.py
+++ split05/question07.py
@@ -2,6 +2,6 @@ def divide(numerator, denominator):
     try:
         return f'Result is {numerator / denominator}'
     except ZeroDivisionError:
-        return f'Oops, {numerator} / {denominator}, division by zero is error!!!'
+        return f'Oops, {numerator}/{denominator}, division by zero is error!!!'
     except TypeError:
         return 'Value Error! You did not enter a number!'
