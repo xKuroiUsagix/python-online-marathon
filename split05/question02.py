14e89b585f75df6b3c2fb26c2baa13911846447b
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-02-18 11:29:03 +0200

    Question 2 - Solved

---
+++ split05/question02.py
@@ -0,0 +1,18 @@
+def day_of_week(day: int):
+    try:
+        int(day)
+        days = {
+            1: 'Monday',
+            2: 'Tuesday',
+            3: 'Wednesday',
+            4: 'Thursday',
+            5: 'Friday',
+            6: 'Saturday',
+            7: 'Sunday'
+        }
+
+        return days[day]
+    except KeyError:
+        return 'There is no such day of the week! Please try again.'
+    except ValueError:
+        return 'You did not enter a number! Please try again.'
