5b18f3f5ecb9769fc74b4ef2bec14831da813124
Vitaliy Povoroznyk <povoroznikvitalii@gmail.com>
2021-03-02 12:56:14 +0200

    Fixed discriminant not 0 bug

--- split08/question03.py
+++ split08/question03.py
@@ -8,7 +8,7 @@ def quadratic_equation(a, b, c):
     D = b**2 - 4*a*c
     if D < 0:
         return None
-    elif not D:
+    elif D == 0:
         return (-b + D**0.5) / (2*a)
 
     x1 = (-b + D**0.5) / (2*a)
