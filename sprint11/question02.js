c1d8468851adddfd28304412de1be46d3e62d386
xKuroiUsagix <povoroznikvitalii@gmail.com>
2021-03-11 16:42:34 +0200

    Question 2 - Solved

---
+++ sprint11/question02.js
@@ -0,0 +1,21 @@
+const howMuchSec = (...params) => {
+    let seconds = params[0] || 0;
+    for (let i = 1; i < params.length; i++) {
+        if (i === 1) {
+            seconds += params[i] * 60;
+        }
+        else if (i === 2) {
+            seconds += params[i] * 60 * 60;
+        }
+        else if (i === 3) {
+            seconds += params[i] * 60 * 60 * 24;
+        }
+        else if (i === 4) {
+            seconds += params[i] * 60 * 60 * 24 * 30;
+        }
+        else if (i == 5) {
+            seconds += params[i] * 60 * 60 * 24 * 30 * 12;
+        }
+    }
+    return seconds;
+}
